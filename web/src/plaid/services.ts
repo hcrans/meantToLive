2c0f9611012bfd3140c5970aa4e68e371c01c8d9
henry@gmail.com <henry@gmail.com>
2024-11-30 03:04:27 +0000

    support for docker

--- web/src/plaid/services.ts
+++ web/src/plaid/services.ts
@@ -1,23 +1,24 @@
 const apiUrl = `http://${window.location.hostname}:${import.meta.env.VITE_API_PORT}`
+console.log({ apiUrl })
 
-export async function createLinkToken() {
+export async function createLinkToken () {
   const res = await fetch(`${apiUrl}/plaid/create_link_token`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const data = await res.json();
-  return data.link_token;
+      'Content-Type': 'application/json'
+    }
+  })
+  const data = await res.json()
+  return data.link_token
 };
 
-export async function storeLinkToken(plaidToken: string) {
+export async function storeLinkToken (plaidToken: string) {
   const result = await fetch(`${apiUrl}/plaid/exchange_public_token`, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({ public_token: plaidToken }),
     headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  return result.ok;
+      'Content-Type': 'application/json'
+    }
+  })
+  return result.ok
 }
